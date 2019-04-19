d8e41f9b8864f6a90d3f424a09bec407166d6799
jessicawright <wright.983@gmail.com>
2019-04-19 12:08:41 -0400

    search results page has background and button styling

--- pitch-for-good-front/js/components/VolunteerList.js
+++ pitch-for-good-front/js/components/VolunteerList.js
@@ -4,17 +4,20 @@ export default function VolunteerList(organization, skill, volunteers) {
     console.log(organization.organizationId)
     console.log(skill.skillId)
     return `
-    ${volunteers.map(volunteer => {
-        return `
-        <h2 class="volunteer" id="${volunteer.volunteerId}">${volunteer.firstName} ${volunteer.lastName}</h2>
-        <h3 class ="volunteer__email">${volunteer.email}</h3>
+    <div class="vol-search">
+        <button class="js-search-again" id="${organization.organizationId}">Search Again</button>
+        <button class="js-back-to-org-dashboard" id="${organization.organizationId}">Back to Dashboard</button>
+        ${volunteers.map(volunteer => {
+            return `
+            <div class="volunteer-results">
+                <h2 class="volunteer" id="${volunteer.volunteerId}">${volunteer.firstName} ${volunteer.lastName}</h2>
+                <h3 class ="volunteer__email">${volunteer.email}</h3>
+            </div>
+            `
+        }).join('')}
+        <ul class="volunteers">
+    </div>
         `
-    }).join('')}
-    <ul class="volunteers">
-    
-    <button class="js-search-again" id="${organization.organizationId}">Search Again</button>
-    <button class="js-back-to-org-dashboard" id="${organization.organizationId}">Back to Dashboard</button>
-    `
 }
                         
                            
