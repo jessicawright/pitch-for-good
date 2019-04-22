d8c91b4b015a125a2c433ccfc3016349b9dd35e0
Jason Hochheiser <jsunh98@gmail.com>
2019-04-22 09:22:28 -0400

    addCauses-styling

--- pitch-for-good-front/js/components/addCauses.js
+++ pitch-for-good-front/js/components/addCauses.js
@@ -1,22 +1,28 @@
 export default function addCauses(volunteer, causes) {
     return`
-    <h1>Add additional causes to your profile:</h1>
+
+    <section class="addCauses__background">
+    <h1 class="addCauses__headline">Add additional causes to your profile:</h1>
 
     <div class="causes__container">
-                    <ul id="causes">
+                    <ul id="cause__container__list">
                         ${causes.map(cause => {
                             return `
-                                <li class="cause">
-                                    <input type="checkbox" class="cause__causeName" id="${cause.causeId}" name="causeIds" value="${cause.causeId}">${cause.causeName}
+                                <li class="causeNameList">
+                                    <label><input type="checkbox" class="cause__causeName" id="${cause.causeId}" name="causeIds" value="${cause.causeId}">${cause.causeName}</label>
+                                </li>
+                                <li class="causeDescpriptionList">
                                     <p class="cause__description">${cause.causeDescription}</p>
                                 </li>
                             `;
                         }).join('')}
                     </ul>
 
-
-    <button id=${volunteer.volunteerId} class="js-addCauses">Submit</button>
-    <button id=${volunteer.volunteerId} class="js-back-to-dashboard">Cancel</button>
+      <div class="cause__buttons">
+        <button id=${volunteer.volunteerId} class="js-addCauses">Submit</button>
+        <button id=${volunteer.volunteerId} class="js-back-to-dashboard">Cancel</button>
+      </div>
     </div>
+  </section>
     `;
 }
