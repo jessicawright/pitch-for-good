42190580acdec96ed96cfdfe9816491f8fb2b2a2
Jason Hochheiser <jsunh98@gmail.com>
2019-04-22 12:03:05 -0400

    initializer adjustment

--- pitch-for-good-front/dist/app.c3f9f951.js
+++ pitch-for-good-front/dist/app.c3f9f951.js
@@ -976,7 +976,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55133" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59417" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
